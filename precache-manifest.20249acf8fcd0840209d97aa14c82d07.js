cbf0c331601b66cfb59c5c513ed304c0aaeb7c0f
Sahil Gufran <sahilgufran2018@gmail.com>
2022-02-18 17:08:56 +0530

    Updates

---
+++ precache-manifest.20249acf8fcd0840209d97aa14c82d07.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9ea148550350ba92629e10640235095",
+    "url": "/liveNotes/index.html"
+  },
+  {
+    "revision": "a6f56906094e9cf80d55",
+    "url": "/liveNotes/static/js/2.20fe2584.chunk.js"
+  },
+  {
+    "revision": "e7719e0e8fa347f0911f",
+    "url": "/liveNotes/static/js/main.4a97cf15.chunk.js"
+  },
+  {
+    "revision": "fdb4e31d319915ff4521",
+    "url": "/liveNotes/static/js/runtime-main.99b2f54f.js"
+  }
+]);
